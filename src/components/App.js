94bf7905a18c7571e88614a8cee77917858958b1
gillysuit <contemporarygil@gmail.com>
2019-09-05 10:21:11 -0700

    added linting and formatting

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-class App extends Compnent {
+class App extends Component {
   render() {
     return (
       <div>
-        <h1>Check, check </h1>
+        <h1>Check, check</h1>
       </div>
-    )
+    );
   }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,30 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-  target: 'web',
+  context: __dirname,
+  devtool: "cheap-eval-source-map",
+  target: "web",
   mode: process.env.NODE_ENV,
   entry: {
-    main: './src/index.js',
+    main: "./src/index.jsx"
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve('./dist'),
+    filename: "bundle.js",
+    path: path.resolve("./dist")
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"]
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: true
   },
   devServer: {
     proxy: {
-      '/api': {
+      "/api": {
         target: 8080,
-        publicPath: '/dist',
+        publicPath: "/dist",
         historyApiFallback: true
       }
     }
@@ -22,19 +32,28 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: ['node_modules'],
-        use: [{ 
-          loader: 'babel-loader'
-        }, {
-          loader: 'css-loader'
-      }],
+        enforce: "pre",
+        test: /\.jsx?$/,
+        loader: "eslint-loader",
+        exclude: /node_modules/
+      },
       {
-        test: /\.scss$/,
-        exclude: /(node_modules)/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      }
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-react"]
+            }
+          }
+        ]
+        // {
+        //   test: /\.scss$/,
+        //   exclude: /(node_modules)/,
+        //   use: ['style-loader', 'css-loader', 'sass-loader']
+        // }
       }
     ]
   }
-}
+};
